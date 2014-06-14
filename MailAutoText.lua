c848b8483d2d1fdcf03817458c14f2a7a1bb2540
porten <porten@gmail.com>
2014-06-14 13:39:23 +0200

    Yarr!!!

--- MailAutoText.lua
+++ MailAutoText.lua
@@ -1,14 +1,11 @@
 require "Window"
 require "GameLib"
 require "Apollo"
-require "ChatSystemLib"
  
 --local MailAutoText = Apollo.GetPackage("Gemini:Addon-1.0").tPackage:NewAddon("MailAutoText", false, {})
 
 local MailAutoText = {}
 
-
-----------------------------------------------------------------------
 function MailAutoText:new(o)
 	o = o or {}
 	setmetatable(o, self)
@@ -24,10 +21,17 @@ function MailAutoText:Init()
 	Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
 end
 
-function MailAutoText:OnLoad() 
-	Print("OnLoad called")
-	self.testprop = "testval"
+function MailAutoText:OnLoad()
+	-- TODO: Check if "Mail" is installed (or have been replaced)
+	--Apollo.GetAddon("Mail").luaComposeMail:FindChild("HeaderTitle"):SetText("Nyt og moderne!")
+	Apollo.RegisterEventHandler("MailAddAttachment", "OnMailAddAttachment", self)
 end
+
+function MailAutoText:OnMailAddAttachment()
+	Print("Yarr!")
+	Apollo.GetAddon("Mail")
+end
+
 
 local MailAutoTextInst = MailAutoText:new()
 MailAutoTextInst:Init()
