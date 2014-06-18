d61ed8aeeabac5f21aab0db82a2bd04cbf7bac69
porten <porten@gmail.com>
2014-06-18 09:47:05 +0200

    Dagnammit, forgot version indicator again

--- MailAutoText.lua
+++ MailAutoText.lua
@@ -4,7 +4,7 @@ require "GameLib"
 require "Apollo"
 
 local MailAutoText = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:NewAddon("MailAutoText", false, {"Mail"}, "Gemini:Hook-1.0")
-MailAutoText.ADDON_VERSION = {1, 4, 1}
+MailAutoText.ADDON_VERSION = {1, 4, 2}
 
 local L = Apollo.GetPackage("Gemini:Locale-1.0").tPackage:GetLocale("MailAutoText")
 
